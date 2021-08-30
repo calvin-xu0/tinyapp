f4986fae236997211b831cac733e97271e9f7d08
Calvin Xu <calvin.xu.jy@gmail.com>
2021-08-30 21:15:36 +0000

    Created URL submission form and corresponding routes

--- express_server.js
+++ express_server.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let randLower = Math.random().toString(36).slice(-6);
+  let randString = "";
+  for (const character of randLower) {
+    if (Number(character) == character || Math.random() > 0.5) {
+      randString += character;
+    } else {
+      randString += character.toUpperCase();
+    }
+  }
+  return randString;
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +32,13 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
