9718e85d9eb3bf4056471bd8164bcb7d9d627b81
Calvin Xu <calvin.xu.jy@gmail.com>
2021-08-31 17:45:19 +0000

    Handle delete requests on server

--- express_server.js
+++ express_server.js
@@ -54,6 +54,10 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect(`/urls`);
+})
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
