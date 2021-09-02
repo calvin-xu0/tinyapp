86ee4f3f7e1ee9c12d04932058ce645dc46f213a
Calvin Xu <calvin.xu.jy@gmail.com>
2021-09-02 22:27:09 +0000

    Verify user object returned from helper function with unit test

---
+++ test/helpersTest.js
@@ -0,0 +1,32 @@
+const { assert } = require('chai');
+const { retrieveUser } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+  
+};
+describe('retrieveUser', function() {
+  it('should return a user with valid email', function() {
+    const user = retrieveUser("user@example.com", testUsers);
+    const expectedOutput = {
+      id: "userRandomID", 
+      email: "user@example.com", 
+      password: "purple-monkey-dinosaur"
+    };
+    assert.deepEqual(user, expectedOutput);
+  });
+
+  it('should return undefined with an invalid email', function() {
+    const user = retrieveUser("invalid@user.com", testUsers);
+    assert.isUndefined(user);
+  });
+});
